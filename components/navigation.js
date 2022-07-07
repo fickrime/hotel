09cb52dccb595919b5602ed8258048c91e626a58
Sandi Pradana <fikcri.me@gmail.com>
2022-07-07 20:38:49 +0700

    navigasi

--- components/navigation.js
+++ components/navigation.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 function Navigation() {
     return (
         <div className="navigation">
             <div className="container">
-                <a href="index.html">Home</a>
-                <a href="price.html">Price</a>
-                <a href="galery.html">Galery</a>
-                <a href="about.html">About</a>
-                <a href="contact.html">Contact</a>
+                <Link href="/"><a>Home</a></Link>
+                <Link href="/price"><a>Price</a></Link>
+                <Link href="/galery"><a>Galery</a></Link>
+                <Link href="/about"><a>About</a></Link>
+                <Link href="/contact"><a>Contact</a></Link>
             </div>
         </div>)
 }
